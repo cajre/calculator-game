acaff27645124ec22ebcf2fc364025b1375c8c95
cajre <167599628+cajre@users.noreply.github.com>
2024-04-29 16:55:54 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,36 @@
-// Function to generate a random math problem
+var mode = "easy"; // Default mode is easy
+
+// Function to change the mode
+function changeMode() {
+    mode = document.getElementById("mode").value;
+    generateProblem(); // Generate a new problem when mode changes
+}
+
+// Function to generate a random math problem based on the mode
 function generateProblem() {
-    var num1 = Math.floor(Math.random() * 10) + 1;
-    var num2 = Math.floor(Math.random() * 10) + 1;
-    var operator = ['+', '-', '*'][Math.floor(Math.random() * 3)]; // Removed division
+    var num1, num2, operator;
+    switch (mode) {
+        case "easy":
+            num1 = Math.floor(Math.random() * 10) + 1;
+            num2 = Math.floor(Math.random() * 10) + 1;
+            operator = ['+', '-', '*'][Math.floor(Math.random() * 3)];
+            break;
+        case "intermediate":
+            num1 = Math.floor(Math.random() * 100) + 1;
+            num2 = Math.floor(Math.random() * 100) + 1;
+            operator = ['+', '-', '*'][Math.floor(Math.random() * 3)];
+            break;
+        case "hard":
+            num1 = Math.floor(Math.random() * 21) - 10; // Include negatives
+            num2 = Math.floor(Math.random() * 21) - 10; // Include negatives
+            operator = ['+', '-', '*', '^'][Math.floor(Math.random() * 4)]; // Include squares
+            if (operator === '^') {
+                num2 = 2; // Square
+            }
+            break;
+    }
     var problemElement = document.getElementById("problem");
     problemElement.textContent = num1 + " " + operator + " " + num2;
 }
 
-// Function to check the answer
-function checkAnswer() {
-    var problem = document.getElementById("problem").textContent;
-    var answer = parseFloat(document.getElementById("answer").value);
-    var resultElement = document.getElementById("result");
-    var nums = problem.split(" ");
-    var num1 = parseInt(nums[0]);
-    var num2 = parseInt(nums[2]);
-    var operator = nums[1];
-    var correctAnswer;
 
-    switch (operator) {
-        case '+':
-            correctAnswer = num1 + num2;
-            break;
-        case '-':
-            correctAnswer = num1 - num2;
-            break;
-        case '*':
-            correctAnswer = num1 * num2;
-            break;
-    }
-
-    if (answer === correctAnswer) {
-        resultElement.textContent = "Correct! The answer is " + correctAnswer;
-    } else {
-        resultElement.textContent = "Incorrect. Try again!";
-    }
-}
-
-// Function to set a custom problem
-function setCustomProblem() {
-    var customProblem = document.getElementById("customProblem").value;
-    var problemElement = document.getElementById("problem");
-    problemElement.textContent = customProblem;
-}
